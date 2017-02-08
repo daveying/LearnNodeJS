64db9f7b9c0f5e9590e6903c804baaf7f98b1a98
Dave Da <daveying@163.com>
2017-02-08 10:40:19 +0800

    modify server.js and add index.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(7888);
+function start(route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-console.log('Server running at http://127.0.0.1:7888/');
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(7888);
+    console.log("Server has started.");
+    console.log(__filename);
+}
+
+exports.start = start;
