bc962a0b3bc0fc0659133023df961be0f9f5dd4e
Dave Da <daveying@163.com>
2017-02-08 13:20:44 +0800

    modify server2.js

--- server2.js
+++ server2.js
@@ -5,10 +5,11 @@ var url = require('url');
 http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received...");
+    console.log("Request for " + pathname.substr(1) + " received...");
 
     fs.readFile(pathname.substr(1), function (err, data) {
         if (err) {
-            console.log(err);
+            console.log('error msg: ', err);
             response.writeHead(404, {'Content-Type' : 'text/html'});
         }
         else {
